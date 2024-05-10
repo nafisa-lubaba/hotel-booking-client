4c9b5ecb55db39a3c46c24a3eb35780084460928
Acer <nafisalubaba71@gmail.com>
2024-05-10 19:40:59 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root';
+import ErrorPage from './pages/ErrorPage';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+       <RouterProvider router={router} />
+  </React.StrictMode>,
+)
